398cc51d1ca9227ed6a172b780321de73caf4ae0
Gabriel Sobral <103462795+GabrielSobral8@users.noreply.github.com>
2024-04-01 23:40:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+let currentQuestion = 1;
+
+function checkAnswer(questionNumber) {
+  let answer = document.getElementById(`answer${questionNumber}`).value.trim().toLowerCase();
+  let correctAnswer = getCorrectAnswer(questionNumber);
+
+  if (answer === correctAnswer) {
+    document.getElementById(`q${questionNumber}`).style.display = 'none';
+    if (questionNumber < 3) {
+      currentQuestion++;
+      document.getElementById(`q${currentQuestion}`).style.display = 'block';
+    } else {
+      document.getElementById('result').style.display = 'block';
+    }
+  } else {
+    alert('Incorrect answer. Please try again.');
+  }
+}
+
+function getCorrectAnswer(questionNumber) {
+  switch (questionNumber) {
+    case 1:
+      return '08/01';
+    case 2:
+      return 'sprinkles';
+    case 3:
+      return 'linos belly';
+    default:
+      return '';
+  }
+}
+
+function revealSurprise() {
+  document.getElementById('surprise').style.display = 'block';
+  document.getElementById('surprise').innerHTML = 'The surprise is... <a href="https://www.instagram.com/teamomucholittlelion">Te Amo</a>';
+}
